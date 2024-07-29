cedb2d3bab8cc3924768709e8dc1ac0a6c407e9b
Gleb Laychenkov <racers2no95@mail.ru>
2024-07-29 19:04:04 +0400

    initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,8 @@
 function compareArrays(arr1, arr2) {
-  
+	return arr1.length === arr2.length && arr1.every((number, i) => number === arr2[i])
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, age, index, usersAge) => acc + age / usersAge.length, 0)
+	return result;
 }
